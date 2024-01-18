ccf1b3d8fd4b7ac3151d874c55bd6b9c75499ce9
Akanksha Wattamwar <akankshawattamwar4@gmail.com>
2024-01-18 14:47:44 +0530

    Fixed Stuff, Akanksha

--- src/comp/Navbar/index.jsx
+++ src/comp/Navbar/index.jsx
@@ -3,14 +3,13 @@ import "./style.css";
 import { useState } from "react";
 import logo from "../../assets/logo_bg_removed.png"
 import { HashLink as NavLink } from "react-router-hash-link";
-import About from "../About";
+
 const Navbar = () => {
   const [isOpen, setIsopen] = useState(false);
   const toggle = () => setIsopen(!isOpen);
 
   return (
     <>
-    {/* {about = document.getElementById('about')} */}
     <div>
     <section className="navbar-container z-50" data-scroll-container>
     <div className="nav-logo">
@@ -35,7 +34,7 @@ const Navbar = () => {
               About
             </p>
           </NavLink>
-          <NavLink to="#awards" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
+          <NavLink to="#faculty" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
             <p>
               Awards
             </p>
@@ -66,7 +65,7 @@ const Navbar = () => {
         <div className={`${isOpen? "flex" : "hidden"} md:hidden text-[#f5f0ec] bg-black fixed top-0 pt-40 text-center flex flex-col justify-start min-h-screen z-10 overflow-hidden w-2/3 right-0 rounded-l-[20px]`}>
           <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#" className=" font">Home</NavLink></div>
           <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#about" className=" font">About</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#awards" className=" font">Awards</NavLink></div>
+          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#faculty" className=" font">Awards</NavLink></div>
           <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#events" className=" font">Events</NavLink></div>
           <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#projects" className=" font">Projects</NavLink></div>
           <div className="flex justify-center p-2 text-2xl" onClick={toggle}>
