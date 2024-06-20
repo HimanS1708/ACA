edf1031b6ef450566dcd4ecc4f81625d6066654a
Himan_S <myanshim17@gmail.com>
2024-06-20 22:01:16 +0530

    Home page and Navbar v0, Himan

--- src/comp/Home/index.jsx
+++ src/comp/Home/index.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
-import background from '../../assets/home_bg.png'
+import { useState, useEffect } from 'react';
 import './style.css';
+
 function Home() {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [, setIndex] = useState(1);
+  const toRotate = [ "$ Hello World!", "$ Welcome to ACA, IIT Kanpur!" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text, delta, ])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) || "" : fullText.substring(0, text.length + 1) || "";
+
+    setText(updatedText || "");
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
   return (
-    <section className="home-container pt-20 pb-20 bg-no-repeat bg-center bg-cover" data-scroll-container>
+    <section className="home-container pt-10 pb-20 bg-no-repeat bg-center bg-cover bg-gradient-to-b from-transparent to-slate-300" data-scroll-container>
       <div className="home-heading pt-20">
-        <p className='text-8xl'>$ Hello World!_</p>
+        <p className='text-8xl max-w-'>{text}</p>
       </div>
       <div className="home-paragraph flex justify-center pt-16">
-        <p className='max-w-3xl text-2xl'>Association for Computing Activities (ACA) is the Computer Science society of IIT Kanpur. ACA organizes departmental activities right from freshers’ to farewell in addition to several programming competitions and lectures.</p>
+        <p className='max-w-3xl text-2xl font-medium'>Association for Computing Activities (ACA) is the Computer Science society of IIT Kanpur. ACA organizes departmental activities right from freshers' to farewell in addition to several programming competitions and lectures.</p>
       </div>
     </section>
   )
--- src/comp/Navbar/index.jsx
+++ src/comp/Navbar/index.jsx
@@ -1,82 +1,26 @@
 import React from "react";
 import "./style.css";
-import { useState } from "react";
 import logo from "../../assets/logo_bg_removed.png"
-import { HashLink as NavLink } from "react-router-hash-link";
 
 const Navbar = () => {
-  const [isOpen, setIsopen] = useState(false);
-  const toggle = () => setIsopen(!isOpen);
-
   return (
     <>
-    <div>
-    <section className="navbar-container z-50" data-scroll-container>
-    <div className="nav-logo">
-        <img
-        src={logo}
-        alt="Logo"
-        className="scale-150 w-[4em] "
-        />
+      <section className="navbar-container z-50 flex justify-between bg-transparent" data-scroll-container>
+        <div className="nav-logo flex pl-10 pt-2">
+          <img
+          src={logo}
+          alt="Logo"
+          className="scale-125 w-[4em]"
+          />
+          <p className="text-black text-3xl pl-4 pt-2 font-medium font-serif">ACA</p>
         </div>
-      <nav className="nav1 md:bg-gray-700 z-50">
-      </nav>
-
-      <div className="Nav-components">
-        
-
-        <div className="Nav-in">
-          <NavLink to="#" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
-            <p>Home</p>
-          </NavLink>
-          <NavLink to="#about" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
-            <p>
-              About
-            </p>
-          </NavLink>
-          <NavLink to="#events" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
-            <p>
-              Events
-            </p>
-          </NavLink>
-          <NavLink to="#faculty" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
-            <p>
-              Awards
-            </p>
-          </NavLink>
-          <NavLink to="#projects" smooth duration={500} className="navlink md:cursor-pointer  md:hover:text-blue-400 md:duration-500">
-            <p>
-              Projects
-            </p>
-          </NavLink>  
-        </div>
-        <NavLink to="#contacts" smooth duration={500} className="registerbtn">
-          <button className="nav-reg">
-            <a  className="text-white">Contact Us</a>
-          </button>
-        </NavLink>
-      </div>
-
-      {/* code for small screens */}
-      <div className="menu md:hidden top-6 right-5 absolute text-right z-50 h-[2rem] w-[36px] flex items-center justify-center" itemType="button" onClick={toggle}>
-          {isOpen? (<svg className="w-6 h-6 bg-[#f5f0ec] rounded fixed" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>) : (<svg className="h-8 w-8 fixed rounded" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>)}
-      </div>
-      <div>
-        <div className={`${isOpen? "flex" : "hidden"} md:hidden text-[#f5f0ec] bg-black fixed top-0 pt-40 text-center flex flex-col justify-start min-h-screen z-10 overflow-hidden w-2/3 right-0 rounded-l-[20px]`}>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#" className=" font">Home</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#about" className=" font">About</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#faculty" className=" font">Awards</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#events" className=" font">Events</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}><NavLink smooth to="#projects" className=" font">Projects</NavLink></div>
-          <div className="flex justify-center p-2 text-2xl" onClick={toggle}>
-            <button className="nav-reg">
-            <a href="#contacts">Contact Us</a>
-          </button>
-          </div>
-        </div>
-      </div>
-    </section>
-    </div>
+        <button className="nav-menu bg-black transition ease-in-out hover:bg-slate-900 duration-300 hover:duration-300 flex pl-6">
+          <span className="menu-icon scale-50"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
+          <linearGradient id="9iHXMuvV7brSX7hFt~tsna_Rdp3AydLFY2A_gr1" x1="12.066" x2="34.891" y1=".066" y2="22.891" gradientUnits="userSpaceOnUse"><stop offset=".237" stop-color="#3bc9f3"></stop><stop offset=".85" stop-color="#1591d8"></stop></linearGradient><path fill="url(#9iHXMuvV7brSX7hFt~tsna_Rdp3AydLFY2A_gr1)" d="M43,15H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,14.1,44.1,15,43,15z"></path><linearGradient id="9iHXMuvV7brSX7hFt~tsnb_Rdp3AydLFY2A_gr2" x1="12.066" x2="34.891" y1="12.066" y2="34.891" gradientUnits="userSpaceOnUse"><stop offset=".237" stop-color="#3bc9f3"></stop><stop offset=".85" stop-color="#1591d8"></stop></linearGradient><path fill="url(#9iHXMuvV7brSX7hFt~tsnb_Rdp3AydLFY2A_gr2)" d="M43,27H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,26.1,44.1,27,43,27z"></path><linearGradient id="9iHXMuvV7brSX7hFt~tsnc_Rdp3AydLFY2A_gr3" x1="12.066" x2="34.891" y1="24.066" y2="46.891" gradientUnits="userSpaceOnUse"><stop offset=".237" stop-color="#3bc9f3"></stop><stop offset=".85" stop-color="#1591d8"></stop></linearGradient><path fill="url(#9iHXMuvV7brSX7hFt~tsnc_Rdp3AydLFY2A_gr3)" d="M43,39H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,38.1,44.1,39,43,39z"></path>
+          </svg></span>
+          <span className="text-3xl pr-8 pb-6 pt-[0.35em] text-white">Menu</span>
+        </button>
+      </section>
     </>
   );
 };
